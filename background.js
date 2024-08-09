0bba0de61316975922cc7136acae74bcee1d455c
Saurav Prateek <srvptk97@gmail.com>
2024-08-09 19:06:10 +0530

    Delete tabs from current window on exceeding max tab limit

--- background.js
+++ background.js
@@ -1,7 +1,10 @@
 const MAX_TABS = 10;
 
 chrome.tabs.onCreated.addListener(async () => {
-  const tabs = await chrome.tabs.query({});
+  const tabs = await chrome.tabs.query({
+    // Gets all the tabs from the current window.
+    currentWindow: true,
+  });
   if (tabs.length > MAX_TABS) {
     const sortedTabs = tabs.sort((a, b) => b.id - a.id);
     const tabsToClose = sortedTabs.slice(MAX_TABS);
